952095d2427f0006af972a6068bb6c90ae3213fc
Sebastian Sebald <sebastian.sebald@reservix.de>
2019-03-18 10:00:54 +0100

    Add Typescript Typings

---
+++ index.d.ts
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+export type InkTextInputProps = {
+	/**
+	 * Value to display in a text input.
+	 */
+	value: string;
+
+	/**
+	 * Text to display when `value` is empty.
+	 */
+	placeholder?: string;
+
+	/**
+	 * Listen to user's input. Useful in case there are multiple input components
+	 * at the same time and input must be "routed" to a specific component.
+	 */
+	focus?: boolean;
+
+	/**
+	 * Replace all chars and mask the value. Useful for password inputs.
+	 */
+	mask?: string;
+
+	/**
+	 * Whether to show cursor and allow navigation inside text input with arrow keys.
+	 */
+	showCursor?: boolean;
+
+	/**
+	 * Function to call when value updates.
+	 */
+	onChange: (value:string) => void;
+
+	/**
+	 * Function to call when `Enter` is pressed, where first argument is a value of the input.
+	 */
+	onSubmit?: (value:string) => void;
+};
+
+declare class InkTextInput extends React.Component<InkTextInputProps> {}
+
+export default InkTextInput;
