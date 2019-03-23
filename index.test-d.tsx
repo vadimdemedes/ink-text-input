e4af0a5a94b4c33ec667e6937c1fc45b4f1578a2
Sebastian Sebald <sebastian.sebald@gmail.com>
2019-03-23 05:13:27 +0100

    Add Typescript Typings (#23)
    * Add Typescript Typings
    
    * integrate comments
    
    * Update index.test-d.tsx
    
    * Add typecheck to test script

---
+++ index.test-d.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import InkTextInput from '.';
+
+const handler = (value: string) => console.log(value);
+
+const Input = () => <InkTextInput value="foo" onChange={handler} />;
+const AllPropsInput = () => (
+	<InkTextInput
+		value="bar"
+		placeholder="placeholder"
+		focus={false}
+		mask="*"
+		showCursor
+		onChange={handler}
+		onSubmit={handler}
+	/>
+);
